89a44f56adad6c9eca2aefde00be9cfd55a61b4b
ulungfikri <ulung.fikri@gmail.com>
2018-11-22 09:52:50 +0700

    delete

--- index.php
+++ index.php
@@ -113,6 +113,79 @@ require_once("app-code.php");
 								</ul>
 							</div>
 						</div>
+
+												<div class="col-md-3 resent-grid recommended-grid">
+							<div class="resent-grid-img recommended-grid-img">
+								<a href="play.php"><img src="images/r1.jpg" alt="" /></a>
+								<div class="time small-time">
+									<p>2:34</p>
+								</div>
+								<div class="clck small-clck">
+									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+								</div>
+							</div>
+							<div class="resent-grid-info recommended-grid-info video-info-grid">
+								<h5><a href="play.php" class="title">Varius sit sed viverra viverra nullam nullam interdum metus</a></h5>
+								<ul>
+									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-3 resent-grid recommended-grid">
+							<div class="resent-grid-img recommended-grid-img">
+								<a href="play.php"><img src="images/r2.jpg" alt="" /></a>
+								<div class="time small-time">
+									<p>3:02</p>
+								</div>
+								<div class="clck small-clck">
+									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+								</div>
+							</div>
+							<div class="resent-grid-info recommended-grid-info video-info-grid">
+								<h5><a href="play.php" class="title">Nullam interdum metus viverra nullam varius sit sed viverra</a></h5>
+								<ul>
+									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-3 resent-grid recommended-grid">
+							<div class="resent-grid-img recommended-grid-img">
+								<a href="play.php"><img src="images/r3.jpg" alt="" /></a>
+								<div class="time small-time">
+									<p>1:34</p>
+								</div>
+								<div class="clck small-clck">
+									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+								</div>
+							</div>
+							<div class="resent-grid-info recommended-grid-info video-info-grid">
+								<h5><a href="play.php" class="title">Varius sit sed viverra nullam viverra nullam interdum metus</a></h5>
+								<ul>
+									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-3 resent-grid recommended-grid">
+							<div class="resent-grid-img recommended-grid-img">
+								<a href="play.php"><img src="images/r4.jpg" alt="" /></a>
+								<div class="time small-time">
+									<p>2:09</p>
+								</div>
+								<div class="clck small-clck">
+									<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+								</div>
+							</div>
+							<div class="resent-grid-info recommended-grid-info video-info-grid">
+								<h5><a href="play.php" class="title">Nullam interdum viverra nullam metus varius sit sed viverra</a></h5>
+								<ul>
+									<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+									<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+								</ul>
+							</div>
+						</div>
 						<div class="clearfix"> </div>
 					</div>		
 				</div>
